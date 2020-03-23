8bc00e27e820ca406140d5689168caa7c06cb78f
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-23 13:54:45 -0700

    Finished up endpoints
    *3 endpoints completed on usersRouter.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
-const userRouter = require("");
+const userRouter = require("../users/usersRouter");
 
 const server = express();
 
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -3,10 +3,36 @@ const Users = require("./usersModel");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+  Users.add(req.body)
+    .then(() => {
+      res.status(201).json({ message: "successfully created an account." });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Error when adding user." });
+    });
+});
 
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-router.get("/users", (req, res) => {});
+  Users.findBy({ username })
+    .then((response) => {
+      res.status(200).json({ username: response.username });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Invalid Credentials" });
+    });
+});
+
+router.get("/users", (req, res) => {
+  Users.find()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch(() => {
+      res.status(400).json({ message: "You shall not pass" });
+    });
+});
 
 module.exports = router;
