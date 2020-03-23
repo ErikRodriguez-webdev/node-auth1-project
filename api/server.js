85c7d360064cb079c52db510d42687d069032d89
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-23 16:35:18 -0700

    fixed router issue with cookie restricted middleware

--- api/server.js
+++ api/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const helmet = require("helmet");
 const userRouter = require("../users/usersRouter");
+const session = require("express-session");
 
 const server = express();
 
+const sessionConfig = {
+  name: "cookie",
+  secret: "keep a secret",
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: true
+};
+
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.get("/", (req, res) => {
   res.send(`<h1>API IS UP!</h1>`);
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const Users = require("./usersModel");
+const bcrypt = require("bcryptjs");
+const restricted = require("../auth/restricted-middleware");
 
 const router = express.Router();
 
 router.post("/register", (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
+
   Users.add(req.body)
     .then(() => {
       res.status(201).json({ message: "successfully created an account." });
@@ -17,15 +24,25 @@ router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   Users.findBy({ username })
-    .then((response) => {
-      res.status(200).json({ username: response.username });
+    .then((user) => {
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = {
+          id: user.id,
+          username: user.username
+        };
+        console.log("its here", req.session.user);
+        res.status(200).json({ username: user.username });
+      } else {
+        res.status(401).json({ message: "User is not authorized." });
+      }
     })
     .catch(() => {
       res.status(500).json({ message: "Invalid Credentials" });
     });
 });
 
-router.get("/users", (req, res) => {
+router.get("/users", restricted, (req, res) => {
   Users.find()
     .then((response) => {
       res.status(200).json(response);
